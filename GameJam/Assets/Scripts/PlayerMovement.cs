d741b315ddee8ae4827799b8100f58450799f3b0
Diego Rivas <diego.rivas@ucsp.edu.pe>
2020-02-01 10:52:52 -0500

    Move

---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controler;
+    public float runSpeed = 40f;
+    float HorizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controler.Move(HorizontalMove*Time.fixedDeltaTime,false,false);
+    }
+}
