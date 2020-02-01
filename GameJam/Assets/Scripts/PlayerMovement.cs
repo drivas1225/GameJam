6a630be37d61794969ed9f1ba8d45c35984aa11c
Diego Rivas <diego.rivas@ucsp.edu.pe>
2020-02-01 11:17:52 -0500

    Update player movements

--- GameJam/Assets/Scripts/PlayerMovement.cs
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,22 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controler;
     public float runSpeed = 40f;
     float HorizontalMove = 0f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
         HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
     {
-        controler.Move(HorizontalMove*Time.fixedDeltaTime,false,false);
+        controler.Move(HorizontalMove*Time.fixedDeltaTime,false,jump);
+        jump = false;
     }
 }
