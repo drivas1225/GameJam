1255249fef3423a859e0d194c9490d8f67268c81
Diego Rivas <diego.rivas@ucsp.edu.pe>
2020-02-02 15:52:14 -0500

    last update

---
+++ GameJam/Assets/ReturnToMeu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMeu : MonoBehaviour
+{
+    public LayerMask whatIsPlayer;
+    RaycastHit2D hit;
+    [SerializeField] GameObject displayText;
+    [SerializeField] bool value;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector2 rayVector = new Vector2(1, -0.5f * transform.localScale.x);
+        hit = Physics2D.Raycast(transform.position, rayVector * transform.localScale.x, 0.001f, whatIsPlayer);
+   
+        if (hit.collider != null)
+        {
+            displayText.SetActive(value);
+        }
+    }
+}
