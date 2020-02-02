8e89018a20a8fcb6a93b59a723c9196e810ab13e
JRod166 <jose.vilca.vargas@gmail.com>
2020-02-02 08:46:51 -0500

    scene

---
+++ GameJam/Assets/Scripts/OnLoose.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLoose : MonoBehaviour
+{
+    public LayerMask whatIsLimit;
+    RaycastHit2D hit;
+    [SerializeField] GameObject rewindText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 rayVector = new Vector2(1, -0.5f * transform.localScale.x);
+        hit = Physics2D.Raycast(transform.position, rayVector * transform.localScale.x, 0.01f, whatIsLimit);
+        if (hit.collider != null)
+        {
+            rewindText.SetActive(true);
+        }
+        else
+        {
+            rewindText.SetActive(false);
+        }
+    }
+}
